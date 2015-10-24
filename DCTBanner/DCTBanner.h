bc4b269cfbd79d296f2951c82a8866fee61f70a8
Daniel Tull <dt@danieltull.co.uk>
2015-10-24 11:05:30 +0100

    Add configurations and demo app

--- DCTBanner/DCTBanner.h
+++ DCTBanner/DCTBanner.h
@@ -1,19 +1,5 @@
-//
-//  DCTBanner.h
-//  DCTBanner
-//
-//  Created by Daniel Tull on 24.10.2015.
-//  Copyright © 2015 Daniel Tull. All rights reserved.
-//
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
-//! Project version number for DCTBanner.
 FOUNDATION_EXPORT double DCTBannerVersionNumber;
-
-//! Project version string for DCTBanner.
 FOUNDATION_EXPORT const unsigned char DCTBannerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <DCTBanner/PublicHeader.h>
-
-
